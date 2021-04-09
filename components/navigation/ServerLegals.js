5ddea642a7a485be959f52aba7818162455ff98c
Ynohtna <45896947+Ynohtna-Fr@users.noreply.github.com>
2021-04-09 17:29:25 +0200

    [Core][#6]Add legals

---
+++ components/navigation/ServerLegals.js
@@ -0,0 +1,9 @@
+
+export default function ServerLegals () {
+
+    return (
+        <footer className="legals">
+            <a href="/legals" target="_BLANK">Mentions légales</a>
+        </footer>
+    )
+}
---
+++ pages/legals.js
@@ -0,0 +1,38 @@
+export default function Legals () {
+    return (
+        <div className="legalsPage">
+            <div>
+            <h1>En vrai c'est un peu chiant ici, c'est les mentions légales.</h1>
+            <p>Vous n'êtes pas obligé de tout lire, mais si vous décider de le faire, sachez qu'une longue route vous attend. Bonne chance dans votre aventure mousallions !</p>
+            </div>
+            <br/><br/>
+            <p>Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des Utilisateurs du site https://anthony-adam.fr les présentes mentions légales.
+                La connexion et la navigation sur le site https://anthony-adam.fr par l’Utilisateur implique acceptation intégrale et sans réserve des présentes mentions légales.
+                Ces dernières sont accessibles sur le site à la rubrique « Mentions légales ».</p>
+
+
+            <h2>ARTICLE 1 : L’éditeur </h2>
+            <p>L’édition et la direction de la publication du site https://anthony-adam.fr est assurée par Anthony, domiciliée 3 rue du houblon, dont le numéro de téléphone est 0652728018, et l'adresse e-mail contact@anthony-adam.fr.</p>
+            <p>En cas de réclamation sur le contenu de ce site, commentaire ou publication, je vous propose de m’adresser un courrier électronique "contact@anthony-adam.fr".</p>
+            <p>a loi vous permet même de vous adresser directement à l’hébergeur sous réserve de l’article 6, I, 4° de la loi 2004-575 du 21 juin 2004 ("Le fait, pour toute personne, de présenter aux [hébergeurs du site] un contenu ou une activité comme étant illicite dans le but d’en obtenir le retrait ou d’en faire cesser la diffusion, alors qu’elle sait cette information inexacte, est puni d’une peine d’un an d’emprisonnement et de 15 000 EUR d’amende."). </p>
+
+
+            <h2>ARTICLE 2 : L’hébergeur</h2>
+
+            <p>L'hébergeur du site https://anthony-adam.fr est la Société Vercel Inc., dont le siège social est situé au 340 S Lemon Ave #4133 Walnut CA 91789 United States  , avec le numéro de téléphone : +1 951-383-6898.</p>
+
+            <h2>ARTICLE 3 : Accès au site</h2>
+
+            <p>Le site est accessible par tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption programmée ou non et pouvant découlant d’une nécessité de maintenance.
+            En cas de modification, interruption ou suspension des services le site https://anthony-adam.fr ne saurait être tenu responsable.</p>
+
+            <h2>ARTICLE 4 : Collecte des données</h2>
+
+            <p>Le site est exempté de déclaration à la Commission Nationale Informatique et Libertés (CNIL) dans la mesure où il ne collecte aucune donnée concernant les utilisateurs. <em>(Ouai j'suis comme ça, me remercier pas !)</em></p>
+
+            <h2>ARTICLE 6 : Propriété intellectuelle</h2>
+
+            <p>J'ai copié discord <em>(pas le code)</em>, donc ce serait l'hôpital qui se fout de la charité de vous dire de ne pas copier ! Par contre, le code lui reste protégé <em>(mais rien ne vous empêche de changer 2-3 classes pour passer inaperçu ;) )</em></p>
+        </div>
+    )
+}
