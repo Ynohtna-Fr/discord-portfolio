4cb55bcdb6774dcd709fd884608d225c4030f85d
YnothnaFr <aazstudio01@gmail.com>
2021-08-05 21:57:33 +0200

    [Core][#14] Add Ackee Analytics

--- components/room/channel/goals/Goal2021.js
+++ components/room/channel/goals/Goal2021.js
@@ -9,7 +9,7 @@ export default function Goal2021 ({ name }) {
           <p>Chaque année je fais une liste d'objectif pour l'année en cours. Voici mes objectifs 2021 !</p>
           <br/>
           <ul>
-            <li><img src="/img/emoji/pushpin.svg" className="emoji-s" alt="emoji épingle"/> Continuer mes études dans l'ingénierie </li>
+            <li><img src="/img/emoji/pushpin.svg" className="emoji-s" alt="emoji épingle"/> Continuer mes études dans l'ingénierie <img src="/img/emoji/check.png" className="emoji-s" alt="emoji validé"/></li>
             <li><img src="/img/emoji/pushpin.svg" className="emoji-s" alt="emoji épingle"/> Refaire mon CV en ligne <img src="/img/emoji/check.png" className="emoji-s" alt="emoji validé"/></li>
             <li><img src="/img/emoji/pushpin.svg" className="emoji-s" alt="emoji épingle"/> Approfondir React <img src="/img/emoji/check.png" className="emoji-s" alt="emoji validé"/></li>
             <li><img src="/img/emoji/pushpin.svg" className="emoji-s" alt="emoji épingle"/> Pousser DiscAds en production</li>
--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,13 @@ import '../styles/globals.css'
 import {
   RecoilRoot,
 } from 'recoil';
+import Script from 'next/dist/client/experimental-script'
 
 function MyApp({ Component, pageProps }) {
+
   return (
     <RecoilRoot>
+      <Script async src="https://stats.anthony-adam.fr/tracker.js" data-ackee-server="https://stats.anthony-adam.fr" data-ackee-domain-id="79c45307-311c-4080-8461-84e4d92fcb71" />
       <Component {...pageProps} />
     </RecoilRoot>
   )
